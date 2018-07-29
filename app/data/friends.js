3b128b7d11328aa495cc4830be866592e3b21aef
Lindsay <lindnerdesign@gmail.com>
2018-07-29 17:27:18 -0500

    inital commit

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+
+var friendList = [
+{
+    "name":"Luna Lindner",
+    "photo":"https://pbs.twimg.com/profile_images/948761950363664385/Fpr2Oz35_400x400.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+]
