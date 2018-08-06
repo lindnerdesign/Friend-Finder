5165d78bbee8ba97e111a34ad988ffc5710fe7ab
Lindsay <lindnerdesign@gmail.com>
2018-08-06 16:27:53 -0500

    more tweeks

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,9 +2,9 @@
 var friendList = [
 
     {
-    "name":"Luna A. Doggo",
-    "photo":"https://media.giphy.com/media/S2S0ZDytY6yDm/giphy.gif",
-    "scores":[
+    name: "Luna A. Doggo",
+    photo : "https://img.buzzfeed.com/buzzfeed-static/static/2016-09/30/17/asset/buzzfeed-prod-fastlane03/sub-buzz-1828-1475270901-7.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    scores: [
         1,
         1,
         1,
@@ -19,9 +19,9 @@ var friendList = [
     },
 
     {
-    "name":"Grump E. Cat",
-    "photo":"https://media.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
-    "scores":[
+    name: "Grump E. Cat",
+    photo:"https://amp.thisisinsider.com/images/5ad89bc98a376d50008b462f-750-562.jpg",
+    scores: [
         3,
         1,
         4,
