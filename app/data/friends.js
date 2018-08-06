4efd3031dbf32b130556c73bf720d2e1a9e2980f
Lindsay <lindnerdesign@gmail.com>
2018-08-06 16:06:43 -0500

    bug fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,38 +3,37 @@ var friendList = [
 
     {
     "name":"Luna A. Doggo",
-    "photo":"https://media.giphy.com/media/kudIERso2pFiE/giphy.gif",
+    "photo":"https://media.giphy.com/media/S2S0ZDytY6yDm/giphy.gif",
     "scores":[
-        '1',
-        '1',
-        '1',
-        '2',
-        '5',
-        '4',
-        '2',
-        '5',
-        '2',
-        '4'
+        1,
+        1,
+        1,
+        2,
+        5,
+        4,
+        2,
+        5,
+        2,
+        4
       ]
     },
 
     {
-    "name":"Scared E. Cat",
-    "photo":"https://media.giphy.com/media/4nUjFdHFdHQiY/giphy.gif",
+    "name":"Grump E. Cat",
+    "photo":"https://media.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
     "scores":[
-        '3',
-        '1',
-        '4',
-        '4',
-        '2',
-        '1',
-        '2',
-        '1',
-        '2',
-        '1'
+        3,
+        1,
+        4,
+        4,
+        2,
+        1,
+        2,
+        1,
+        2,
+        1
       ]
     }, 
-
 ]
 
 module.exports = friendList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,36 +8,34 @@ module.exports = function (app) {
   });
 
   // ---------------------------------------------------------------------------
-
-  app.post("/api/friends", function (req, res) {
-
-  var newFriendScores = req.body.scores;
-    // console.log(newFriendScores);
-
-  var scoresMatched = [];
-  var closestMatch = 0;
-
-  //Compare the two friends scores (arrays)
-  for (var i = 0; i < friendList.length; i++) {
     
-    var scoreDifference = 0;
+      app.post("/api/friends", function(req, res) {
+        // console.log(req.body.scores);
+    
+        var user = req.body;
+    
+        for(var i = 0; i < user.scores.length; i++) {
+          user.scores[i] = parseInt(user.scores[i]);
+        }
+    
+        var bestFriendIndex = 0;
+        var minimumDifference = 40;
+    
+        for(var i = 0; i < friendList.length; i++) {
+          var totalDifference = 0;
+          for(var j = 0; j < friendList[i].scores.length; j++) {
+            var difference = Math.abs(user.scores[j] - friendList[i].scores[j]);
+            totalDifference += difference;
+          }
+    
+          if(totalDifference < minimumDifference) {
+            bestFriendIndex = i;
+            minimumDifference = totalDifference;
+          }
+        }
+    
+        friendList.push(user);
 
-    for (var x = 0; x < newFriendScores.length; x++) {
-      scoreDifference += (Math.abs(parseInt(friendList[i].scores[x])) - (parseInt(newFriendScores[x])));
-    }
-    scoresMatched.push(scoreDifference);
-  }
-
-  //find the two closest match
-  for (var j = 0; j < scoresMatched.length; j++) {
-    if (scoresMatched[j] <= scoresMatched[closestMatch]){
-      closestMatch = j;
-    }
-  }
-  
-  friendList.push(req.body);
-
-  var bestFriend = friendList[closestMatch];
-  res.json(bestFriend);
-  });
-};
+        res.json(friendList[bestFriendIndex]);
+      });
+    };
